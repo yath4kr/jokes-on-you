d169715b1d7252982b9c2a91aab2ee79ec92bcda
yatharth <76203406+yath4kr@users.noreply.github.com>
2023-08-02 09:19:14 +0530

    Added files via upload

---
+++ src/Joke.js
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query'
+// import axios from Axios;
+import Axios from 'axios';
+
+export const Joke = () => {
+
+    const { data, isLoading, isError, refetch } = useQuery(['joke'], () => {
+        return Axios.get('https://v2.jokeapi.dev/joke/Any?type=single').then((res) => {
+            return res.data;
+        })
+    })
+
+    if (isError) {
+        return <h1>Oops unfortunately, somthing went wrong try again later :( </h1>
+    }
+
+    if (isLoading) {
+        return <h1>Loading..., Please Wait</h1>
+    }
+
+    return (
+        <>
+            <h1>{data?.joke}</h1>
+            <button onClick={refetch}>Upddate</button>
+        </>
+
+    )
+}
