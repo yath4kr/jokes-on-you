891c1aed30963d4e636732c6f6f2b2d19138584a
unknown <yath4kr@gmail.com>
2023-08-27 14:51:34 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Joke } from './Joke';
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Joke } from "./Joke";
 
 function App() {
   const client = new QueryClient({
     defaultOptions: {
       queries: {
-        refetchOnWindowFocus: false
-      }
-    }
+        refetchOnWindowFocus: false,
+      },
+    },
   });
   return (
     <div className="App">
-      <h1>Here is A Joke, Enjoy ;-) </h1>
+      <h1 className="heading">Here is A Joke, Enjoy ;-) </h1>
       <QueryClientProvider client={client}>
         <Joke />
       </QueryClientProvider>
--- src/Joke.js
+++ src/Joke.js
@@ -1,28 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
 // import axios from Axios;
-import Axios from 'axios';
+import Axios from "axios";
 
 export const Joke = () => {
+  const { data, isLoading, isError, refetch } = useQuery(["joke"], () => {
+    return Axios.get("https://v2.jokeapi.dev/joke/Any?type=single").then(
+      (res) => {
+        return res.data;
+      }
+    );
+  });
 
-    const { data, isLoading, isError, refetch } = useQuery(['joke'], () => {
-        return Axios.get('https://v2.jokeapi.dev/joke/Any?type=single').then((res) => {
-            return res.data;
-        })
-    })
+  if (isError) {
+    return <h1>Oops unfortunately, somthing went wrong try again later :( </h1>;
+  }
 
-    if (isError) {
-        return <h1>Oops unfortunately, somthing went wrong try again later :( </h1>
-    }
+  if (isLoading) {
+    return <h1>Loading..., Please Wait</h1>;
+  }
 
-    if (isLoading) {
-        return <h1>Loading..., Please Wait</h1>
-    }
-
-    return (
-        <>
-            <h1>{data?.joke}</h1>
-            <button onClick={refetch}>Upddate</button>
-        </>
-
-    )
-}
+  return (
+    <div className="content">
+      <h1>{data?.joke}</h1>
+      <button className="button" onClick={refetch}>
+        Update
+      </button>
+    </div>
+  );
+};
